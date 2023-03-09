94245f7f336c83f29f12a1fe24808f29538a3ab1
enrique palomino <octepc.97@gmail.com>
2023-03-08 23:15:00 -0500

    made a component

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export const CounterApp = () => {
+  const [{ counter1, counter2, counter3 }, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+
+  const handleAdd = () => {
+    setCounter(counter1 + 1);
+  };
+
   return (
     <>
-      <h1>Counter:</h1>
+      <h1>Counter: {counter1}</h1>
       <hr />
 
-      <button>+1</button>
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
