19a5418643cdae757fd7700652c163146aba3640
enrique palomino <octepc.97@gmail.com>
2023-03-12 09:21:12 -0500

    applied useCallback to memorize the state of a function

--- src/06-memos/CallbackHook.jsx
+++ src/06-memos/CallbackHook.jsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ShowIncrement } from "./ShowIncrement";
 
 export const CallbackHook = () => {
   const [counter, setCounter] = useState(10);
 
-  const increment = () => {
-    setCounter(counter + 1);
-  };
+  const incrementFather = useCallback(() => {
+    setCounter((value) => value + 1);
+  }, []);
 
   return (
     <>
       <h1>useCallback Hook: {counter} </h1>
       <hr />
 
-      <ShowIncrement increment={increment} />
+      <ShowIncrement increment={incrementFather} />
     </>
   );
 };
